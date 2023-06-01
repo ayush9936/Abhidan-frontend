a0a1291c8b6e55abad4bfe222d4b31d7a72a2ea0
ayush9936 <ayush9936ru@gmail.com>
2023-06-01 05:57:29 +0530

    done

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const app_config = {
-      back_url:"http://localhost:1300",
-  // back_url: "https://abhidan-sewa-backend.onrender.com/api",
+      // back_url:"http://localhost:1300",
+  back_url: "https://abhidan-sewa-backend.onrender.com",
 };
 export default app_config;
